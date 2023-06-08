7b7265cefe0958f9e33481b103c4d4665c51a831
Quentin Upshaw <qjlu404@gmail.com>
2023-06-08 13:34:22 -0500

    Add project files.

---
+++ Chartdownloader-cs/ChartData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chartdownloader_cs
+{
+	public class ChartData
+	{
+		public string OverallNumber { get; set; }
+		public string Title { get; set; }
+		public string Directors { get; set; }
+		public string WrittenBy { get; set; }
+		public string Released { get; set; }
+
+		public string GetStringList()
+		{
+			return OverallNumber + "\n" + Title + "\n" + Directors
+							  + "\n" + WrittenBy + "\n" + Released + "\n";
+		}
+	}
+}
---
+++ Chartdownloader-cs/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+
+namespace Chartdownloader_cs
+{
+	class Program
+	{
+		public static void Main(string[] args) 
+		{
+			string url = "https://en.wikipedia.org/wiki/List_of_SpongeBob_SquarePants_episodes";
+			var web = new HtmlWeb();
+			// downloading to the target page
+			// and parsing its HTML content
+			var document = web.Load(url);
+			var nodes = document.DocumentNode.SelectNodes("//*[@id='mw-content-text']/div[1]/table[position()>1 and position()<15]/tbody/tr[position()>1]");
+			
+			List<ChartData> episodes = new List<ChartData>();
+			// looping over the nodes 
+			// and extract data from them
+			foreach (var node in nodes)
+			{
+				// add a new Episode instance to 
+				// to the list of scraped data
+				episodes.Add(new ChartData()
+				{
+					OverallNumber = HtmlEntity.DeEntitize(node.SelectSingleNode("th[1]").InnerText),
+					Title = HtmlEntity.DeEntitize(node.SelectSingleNode("td[2]").InnerText),
+					Directors = HtmlEntity.DeEntitize(node.SelectSingleNode("td[3]").InnerText),
+					WrittenBy = HtmlEntity.DeEntitize(node.SelectSingleNode("td[4]").InnerText),
+					Released = HtmlEntity.DeEntitize(node.SelectSingleNode("td[5]").InnerText)
+				});
+			}
+
+			//Console.WriteLine(episodes[0].GetStringList());
+		}
+	}
+}
