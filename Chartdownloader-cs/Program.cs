0b03ec2bc917efe56cf3a111930427bc6d6a6b50
Quentin Upshaw <qjlu404@gmail.com>
2023-06-14 23:54:58 -0500

    Revert "Revert "initial commit""
    This reverts commit 0795d8e157a04ced84e79b14827898a17c453335.

--- Chartdownloader-cs/Program.cs
+++ Chartdownloader-cs/Program.cs
@@ -1,37 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HtmlAgilityPack;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
 
 namespace Chartdownloader_cs
 {
 	class Program
 	{
-		public static void Main(string[] args) 
+		public static void Main() 
 		{
-			string url = "https://en.wikipedia.org/wiki/List_of_SpongeBob_SquarePants_episodes";
-			var web = new HtmlWeb();
-			// downloading to the target page
-			// and parsing its HTML content
-			var document = web.Load(url);
-			var nodes = document.DocumentNode.SelectNodes("//*[@id='mw-content-text']/div[1]/table[position()>1 and position()<15]/tbody/tr[position()>1]");
-			
-			List<ChartData> episodes = new List<ChartData>();
-			// looping over the nodes 
-			// and extract data from them
-			foreach (var node in nodes)
+			try
 			{
-				// add a new Episode instance to 
-				// to the list of scraped data
-				episodes.Add(new ChartData()
-				{
-					OverallNumber = HtmlEntity.DeEntitize(node.SelectSingleNode("th[1]").InnerText),
-					Title = HtmlEntity.DeEntitize(node.SelectSingleNode("td[2]").InnerText),
-					Directors = HtmlEntity.DeEntitize(node.SelectSingleNode("td[3]").InnerText),
-					WrittenBy = HtmlEntity.DeEntitize(node.SelectSingleNode("td[4]").InnerText),
-					Released = HtmlEntity.DeEntitize(node.SelectSingleNode("td[5]").InnerText)
-				});
+				MainAsync().Wait();
 			}
-
-			//Console.WriteLine(episodes[0].GetStringList());
+			catch (AggregateException)
+			{
+				return;
+			}
+		}
+		public static async Task MainAsync()
+		{
+			var cdObj = new Chartdownloader();
+			Task<List<ChartData>> task1 = cdObj.ScrapeChartData();
+			while (cdObj.NextPage() != 0)
+			{
+				Task<List<ChartData>> taskwhile = cdObj.ScrapeChartData();
+				await Task.WhenAll(taskwhile);
+			}
+			await Task.WhenAll(task1);
 		}
 	}
 }
